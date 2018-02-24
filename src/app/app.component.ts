7326abfbe36113fdee179829ed7aea4950af6572
ultrasonicsoft <balramchavan@gmail.com>
2018-02-24 12:00:43 +0530

    source code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import '../assets/login-animation.js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app';  
+
+  ngAfterViewInit() {    
+    (window as any).initialize();
+  }
 }
